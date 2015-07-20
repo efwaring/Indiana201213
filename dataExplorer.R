2649807f1454cd4329c5eaca465eac9f355dbb4f
[efwaring] <efwaring@gmail.com>
2015-07-20 14:48:18 -0500

    Start data exploration code

---
+++ dataExplorer.R
@@ -0,0 +1,8 @@
+# Data exploration for data collected in Marshall County, IN in 2012 and 2013 by
+# Lizz Waring and Scott Holaday. 
+
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(nlme)
+

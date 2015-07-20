6495d97ed92f78b80565eb0a6872390612607200
[efwaring] <efwaring@gmail.com>
2015-07-20 14:52:54 -0500

    data entry

--- dataExplorer.R
+++ dataExplorer.R
@@ -6,3 +6,13 @@ library(tidyr)
 library(ggplot2)
 library(nlme)
 
+soil12 <- read.csv("lachat2012.csv")
+soil13 <- read.csv("lachat2013.csv")
+plants12 <- read.csv("data2012.csv")
+plants13 <- read.csv("data2013.csv")
+
+soil12$year <- 2012
+soil13$year <- 2013
+
+
+soil1 <- soil12 %>% separate(Analyte, into=c("Ammonia","NO3+NO2"))

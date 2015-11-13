3f51bfcba577b17fa6246cfd112163f68bcd1f6e
Dylan W. Schwilk <dylan@schwilk.org>
2015-11-13 13:16:14 -0600

    Rename dataframe in soil script
    `all` is name of a built in function. It is dangerous to overwrite such
    names, yet R does not warn you.

--- scripts/soilN-traits-analysis.R
+++ scripts/soilN-traits-analysis.R
@@ -23,10 +23,10 @@ p2013 <- plants13 %>% group_by(site, rep, month, place, spp) %>%
 # merging the soil data with the plant data
 # have a doubling of soil data as one data point for soil N but 
 # possibly two points for species.
-all <- merge(soil13, p2013, by=c("place","month","rep","site"))
-all$soilN <- all$ammonia.kg + all$no3.no2.kg
-all$ratio <- all$ammonia.kg/all$no3.no2.kg
-all$cn <- all$totC/all$totN
+allsoil <- merge(soil13, p2013, by=c("place","month","rep","site"))
+allsoil$soilN <- allsoil$ammonia.kg + allsoil$no3.no2.kg
+allsoil$ratio <- allsoil$ammonia.kg/allsoil$no3.no2.kg
+allsoil$cn <- allsoil$totC/allsoil$totN
 
 # here I have set up the mixed effects model with soilN nested in place
 # place is a random effect but soilN is fixed, but nested. I want to have 
@@ -36,11 +36,11 @@ all$cn <- all$totC/all$totN
 
 n.lme <- lme(totN ~ soilN + month + spp + soilN:month +
                soilN:spp, random =~soilN|place,
-             data=all, na.action=na.omit)
+             data=allsoil, na.action=na.omit)
 
 anova(n.lme)
 
-ggplot(all, aes(soilN, totN, shape=spp, color=spp)) +
+ggplot(allsoil, aes(soilN, totN, shape=spp, color=spp)) +
   geom_point()+
   facet_grid(month~.)+ 
   scale_color_manual(name="N species",
@@ -49,7 +49,7 @@ ggplot(all, aes(soilN, totN, shape=spp, color=spp)) +
 
 # using lme4 package
 
-n.lmer <- lmer(totN ~ spp + month + soilN +(1|place/soilN), data=all,
+n.lmer <- lmer(totN ~ spp + month + soilN +(1|place/soilN), data=allsoil,
                na.action=na.omit)
 summary(n.lmer)
  
